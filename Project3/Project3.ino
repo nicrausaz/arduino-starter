7321d8a176af960bdcf86d166fc59dd7218610af
nicrausaz <n.crausaz99@gmail.com>
2017-01-31 20:50:06 +0100

    refactor3

--- Project3/Project3.ino
+++ Project3/Project3.ino
@@ -3,42 +3,52 @@ const float baseTemp = 20.0;
 
 void setup() {
   Serial.begin(9600); //open serial port
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
+  for (int i = 3; i <= 5; i++) {
+    pinMode(i, OUTPUT);
+  }
 }
 
 void loop() {
   int sensorValue = analogRead(sensorPin); //get sensor value
-  Serial.print(" value ");
-  Serial.print(sensorValue);
+  float voltage = (sensorValue / 1024.0) * 5.0; //convert sensor value with CAN
+  float temperature = (voltage - .5) * 100; //convert voltage to Celsius
 
-  //convert sensor value with CAN
-  float voltage = (sensorValue / 1024.0) * 5.0;
-  Serial.print(" ,voltage ");
-  Serial.print(voltage);
-
-  //convert voltage to Celsius
-  float temperature = (voltage - .5) * 100;
-  Serial.print(" ,Celsius ");
-  Serial.print(temperature);
-
+  printSerial(sensorValue, voltage, temperature);
   delay(2000);
 
   if (temperature < baseTemp) {
     // Less than 20C -- 1LED
-    digitalWrite(3, HIGH);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
+    lightItUp(1);
+
   } else if (temperature >= baseTemp && temperature <= baseTemp + 4) {
     // Between 20C and 24C -- 2LED
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, LOW);
+    lightItUp(2);
+
   } else {
     // More than 24C -- 3LED
+    lightItUp(3);
+  }
+}
+
+void printSerial(int sensorValue, float voltage, float temperature) {
+  Serial.print("value: ");
+  Serial.println(sensorValue);
+  Serial.print("voltage: ");
+  Serial.println(voltage);
+  Serial.print("Celsius: ");
+  Serial.println(temperature);
+}
+
+void lightItUp(int number) {
+  if (number == 1) {
+    digitalWrite(3, HIGH);
+  } else if (number == 2) {
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+  } else {
     digitalWrite(3, HIGH);
     digitalWrite(4, HIGH);
     digitalWrite(5, HIGH);
   }
 }
+
